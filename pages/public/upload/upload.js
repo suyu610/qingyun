d724bf42324c539c4f2170c83e0a3570cf81ae78
Suyu <uuorb123@gmail.com>
2021-06-05 16:03:02 +0800

    上传功能的逻辑：

--- pages/public/upload/upload.js
+++ pages/public/upload/upload.js
@@ -1,4 +1,5 @@
 // pages/public/upload/upload.js
+let app = getApp()
 Page({
 
   /**
@@ -6,19 +7,26 @@ Page({
    */
   data: {
     checkedNeedKnow: false,
-    fileList: [{
-      url: 'https://img.yzcdn.cn/vant/leaf.jpg',
-      name: '图片1',
-    }, {
-      url: 'http://iph.href.lu/60x60?text=default',
-      name: 'pdf',
-      isImage: false,
-      deletable: true,
-    }, ],
+    //   url: 'http://iph.href.lu/60x60?text=default',
+    //   name: 'pdf',
+    //   isImage: false,
+    //   deletable: true,
+
+    fileList: [],
     chooseTag: true,
     modalName: null,
     selected_tag_list: [],
     selectedCount: 0,
+    showCategoryPopup: false,
+    showUploadProgressPopup: false,
+    uploadProgressValue: 0,
+    gradientColor: {
+      '0%': '#ffe9a7',
+      '100%': '#27b1ff',
+    },
+    upload_step_title: "上传图片",
+    upload_step_detail: "1 / 5",
+    categoryList: app.globalData.categoryList,
     checkbox: [{
       value: 0,
       name: '自制',
@@ -51,26 +59,123 @@ Page({
       hot: false,
     }]
   },
+  onTapShowCategoryPopup: function () {
+    this.setData({
+      showCategoryPopup: true
+    })
+  },
+
+  onCloseUploadProgressPopup:function(){
+    this.setData({
+      showUploadProgressPopup: false
+    })
+  },
+  submitUpload: function () {
+    this.setData({
+      showUploadProgressPopup: true
+    })
+
+    setInterval(() => {
+
+      if(this.data.uploadProgressValue == 20){
+        this.setData({upload_step_detail:"2 / 5"})
+      }
+      if(this.data.uploadProgressValue == 30){
+        this.setData({upload_step_detail:"3 / 5"})
+      }
+      if(this.data.uploadProgressValue == 40){
+        this.setData({upload_step_detail:"4 / 5"})
+      }
+      if(this.data.uploadProgressValue == 50){
+        this.setData({upload_step_detail:"5 / 5"})
+      }
+      if(this.data.uploadProgressValue == 60){
+        this.setData({upload_step_title:"上传资料中",upload_step_detail:""})
+      }
+      if(this.data.uploadProgressValue == 100){
+        this.setData({upload_step_title:"上传完成"})
+      }
+      this.setData({
+        uploadProgressValue : this.data.uploadProgressValue + 1
+      })
+    }, 50);
+  },
+  onCloseCategoryPopup: function () {
+    this.setData({
+      showCategoryPopup: false
+    })
+  },
+  deleteFile: function (e) {
+    const {
+      fileList = []
+    } = this.data;
+    //从start的位置开始向后删除delCount个元素
+    fileList.splice(e.detail.index, 1);
+    this.setData({
+      fileList
+    })
+
+  },
+  // 选择文件
+  afterRead: function (event) {
+    // file是个数组
+    const {
+      file
+    } = event.detail;
+
+    const {
+      fileList = []
+    } = this.data;
+
+    file.forEach(element => {
+      if (element.type == "image") {
+        element.isImage = true
+      }
+
+      fileList.push(element)
+    });
+
+
+
+    this.setData({
+      fileList
+    });
+    // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+    // wx.uploadFile({
+    //   url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
+    //   filePath: file.url,
+    //   name: 'file',
+    //   formData: { user: 'test' },
+    //   success(res) {
+    //     // 上传完成需要更新 fileList
+    //     const { fileList = [] } = this.data;
+    //     fileList.push({ ...file, url: res.data });
+    //     this.setData({ fileList });
+    //   },
+    // });
+  },
   showChooseTagModal(e) {
     this.setData({
       modalName: e.currentTarget.dataset.target
     })
   },
-  jump2DocPreview:function(){
-    push({name:"upload_preview"})
+  jump2DocPreview: function () {
+    push({
+      name: "upload_preview"
+    })
   },
   hideChooseTagModal(e) {
     this.setData({
       modalName: null
     })
   },
-  onChangeNeedKown:function(event){
+  onChangeNeedKown: function (event) {
     this.setData({
       checkedNeedKnow: event.detail,
     });
 
   },
-  confirmChooseTag:function() {
+  confirmChooseTag: function () {
     let selected_tag_list = []
     let items = this.data.checkbox;
     for (let i = 0, lenI = items.length; i < lenI; ++i) {
