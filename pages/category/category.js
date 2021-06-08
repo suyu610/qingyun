4ee29632f5df9120e6bef92aa19c7837e0a4f619
Suyu <uuorb123@gmail.com>
2021-06-08 15:54:49 +0800

    增加课程分类'

--- pages/category/category.js
+++ pages/category/category.js
@@ -3,53 +3,15 @@ import {
   push
 } from '../../utils/router/index.js';
 
-const areaList = {
-
-  /////////////// 分类 ////////////////
-  province_list: {
-    110000: '全部',
-    120000: '公共课程',
-    130000: '电子信息学院',
-
-  },
-  city_list: {
-    110100: '全部',
-    120100: '公共课程',
-    130100: '电子信息专业',
-    130200: '微电子专业',
-  },
-  county_list: {
-    110100: '全部',
-    110101: '大一',
-    110102: '大二',
-    110103: '大三',
-    110104: '大四',
-
-    120100: '全部',
-    120101: '大一',
-    120102: '大二',
-    120103: '大三',
-    120104: '大四',
-
-    130100: '全部',
-    130101: '大一',
-    130102: '大二',
-    130103: '大三',
-    130104: '大四',
-
-    130200: '全部',
-    130201: '大一',
-    130202: '大二',
-    130203: '大三',
-    130204: '大四',
-    // ....
-  },
-};
 
 Page({
   data: {
     hidden: true,
-    areaList:areaList
+    areaList: {
+      province_list: {},
+      city_list: {},
+      county_list: {},
+    }
   },
   jump2DocList: function () {
     push({
@@ -78,6 +40,17 @@ Page({
     for (let i = 0; i < 26; i++) {
       list[i] = String.fromCharCode(65 + i)
     }
+
+    let areaList = {}
+    areaList.province_list = app.globalData.initData.category.collegeMap
+    areaList.city_list = app.globalData.initData.category.majorMap
+    areaList.county_list = app.globalData.initData.category.gradeMap
+
+
+    this.setData({
+      areaList
+    })
+
     this.setData({
       list: list,
       listCur: list[0]
@@ -85,12 +58,12 @@ Page({
   },
   onReady() {
     let that = this;
-    wx.createSelectorQuery().select('.indexBar-box').boundingClientRect(function(res) {
+    wx.createSelectorQuery().select('.indexBar-box').boundingClientRect(function (res) {
       that.setData({
         boxTop: res.top
       })
     }).exec();
-    wx.createSelectorQuery().select('.indexes').boundingClientRect(function(res) {
+    wx.createSelectorQuery().select('.indexes').boundingClientRect(function (res) {
       that.setData({
         barTop: res.top
       })
